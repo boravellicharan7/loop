08edf2af77a1346daecc7e211a376bfc1c100f0e
boravellicharan7 <boravellicharan6@gmail.com>
2024-10-02 19:21:54 +0530

    added

---
+++ loop.js
@@ -0,0 +1,84 @@
+//Question-1:-
+var a = "saturday";
+switch (a) {
+    case "monday":
+        console.log("today is monday");
+        break;
+    case "tuesday":
+        console.log("today is tuesday");
+        break;
+    case "wendesday":
+        console.log("today is wendesday");
+        break;
+    case "thurday":
+        console.log("today is thurday");
+        break;
+    case "friday":
+        console.log("today is friday");
+        break;
+    case "saturday":
+        console.log("today is saturday");
+        break;
+    case "sunday":
+        console.log("today is sunday");
+        break;
+}
+//Question-2:-
+var a = "november";
+switch (a) {
+    case "january":
+        console.log("this is january");
+        break;
+    case "february":
+        console.log("this is february");
+        break;
+    case "march":
+        console.log("this is march");
+        break;
+    case "april":
+        console.log("this is april");
+        break;
+    case "may":
+        console.log("this is may");
+        break;
+    case "june":
+        console.log("this is june");
+        break;
+    case "july":
+        console.log("this is july");
+        break;
+    case "august":
+        console.log("this is august");
+        break;
+    case "september":
+        console.log("this is september");
+        break;
+    case "october":
+        console.log("this is october");
+        break;
+    case "november":
+        console.log("this is november");
+        break;
+    case "december":
+        console.log("this is december");
+        break;
+}
+
+//Question-3:-
+var a = 1; //while statement...
+var b = 10;
+while (a <= b) {
+    if (a % 2 == 0) {
+        console.log(a + "is even number");
+    }
+    a++
+}
+
+//Question-4:-
+var a = 10;
+var limit = 1;
+while (a >= limit) {
+    console.log("this attempt is " + a);
+    a--;
+}
+console.log("wait for 25s to login");
